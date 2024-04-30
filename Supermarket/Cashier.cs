3d6748ce518a5dbf283741ea542bb11d4cb7c45c
Cristo <a2308750@institutmontilivi.cat>
2024-04-30 19:54:24 +0200

    Cashier ver 1

---
+++ Supermarket/Cashier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public class Cashier : Person
+    {
+        private DateTime _joiningDate;
+
+        public Cashier(string id, string fullName, long date) : base(id, fullName)
+        {
+            _joiningDate = new DateTime(date);
+        }
+
+        public int YearsOfService
+        {
+            get { return _joiningDate.Year; }
+        }
+
+        public override double GetRating
+        {
+            get { return (YearsOfService * 360) + (_totalInvoiced * 0.10); }
+        }
+
+        public override void AddPoints(int pointsToAdd)
+        {
+            base._points += pointsToAdd * (YearsOfService + 1);
+        }
+
+        public override string ToString()
+        {
+            return $"DNI/NIE -> {base._id} NOM -> {base.FullName} RATING -> {GetRating}   ANTIGUITAT -> {YearsOfService} VENDES -> {_totalInvoiced} " +
+                $"PUNTS -> {_points} DISPONIBLE -> {base.ToString}";
+        }
+    }
+}
