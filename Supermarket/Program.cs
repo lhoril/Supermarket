d2d8f737c6dfea25cd4f7fba7b13f5da4469077b
Cristo <a2308750@institutmontilivi.cat>
2024-04-30 17:07:02 +0200

    Agregar archivos de proyecto.

---
+++ Supermarket/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine('\u20AC');
+        }
+    }
+}
