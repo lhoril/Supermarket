7c3240ad09026a22d5861f36e77dad4ba6c91534
Arnau Guerra González <a2308738@institutmontilivi.cat>
2024-05-08 15:10:10 +0200

    df

--- Supermarket/CheckOutLine.cs
+++ Supermarket/CheckOutLine.cs
@@ -15,6 +15,8 @@ namespace Supermarket
 
         public CheckOutLine(int num, string FileName, Person cashier, bool active)
         {
+            Customer customer = new Customer("1", "arnau", null);
+            DateTime dateOfPurchase = new DateTime();
             StreamReader sr = new StreamReader(FileName);
             queue = new Queue<ShoppingCart>();
             string line;
@@ -29,7 +31,7 @@ namespace Supermarket
                 camps = line.Split(';');
                 line = sr.ReadLine();
                 Item producte = new Item(num, camps[0], false, 0, Convert.ToInt32(camps[1]), Convert.ToChar(camps[2]), Convert.ToDouble(camps[3]), Convert.ToInt32(camps[4]));
-                this.queue.Append(new ShoppingCart(num, line));
+                this.queue.Append(new ShoppingCart(customer, dateOfPurchase));
             }
         }
     }
